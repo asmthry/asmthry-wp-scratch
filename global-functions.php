cc7981c1388eb68859ea79e3b07842fc03d3fd65
ASMTHRY <asmthry@gmail.com>
2021-08-08 14:33:36 +0530

    RESOURCES LOAD CLASS CHANGES 1) Pass class name to load file.

--- global-functions.php
+++ global-functions.php
@@ -58,13 +58,10 @@ function asmthry_enqueue_style( array $ids = null ) {
  * @param string $post_name - Give resources id as an array.
  */
 function asmthry_create_cpt( string $post_name ) {
-	Asmthry_Load_Resource::include_file( 'cpt' );
+	Asmthry_Load_Resource::include_file( 'Asmthry_Cpt' );
 	/** Check if Asmthry Enqueue class loaded properly */
-	if ( class_exists( 'Asmthry_Cpt' ) ) {
-		if ( ! empty( $post_name ) ) {
-			$cpt = new Asmthry_Cpt( $post_name );
-			add_action( 'init', array( $cpt, 'register_cpt' ) );
-		}
-		return;
+	if ( ! empty( $post_name ) ) {
+		$cpt = new Asmthry_Cpt( $post_name );
+		add_action( 'init', array( $cpt, 'register_cpt' ) );
 	}
 }
