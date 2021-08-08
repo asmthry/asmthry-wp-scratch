71db80cb93b30f06ca3623f8c73ded5a8e1c34d1
ASMTHRY <asmthry@gmail.com>
2021-08-08 14:39:14 +0530

    Fix merge issue

--- global-functions.php
+++ global-functions.php
@@ -71,38 +71,6 @@ function asmthry_enqueue_script( array $ids = null ) {
 	}
 }
 
-/** Register Scripts
- *
- * @param array $resources - Give resources as an array.
- */
-function asmthry_register_script( array $resources = null ) {
-	Asmthry_Load_Resource::include_file( 'enqueue' );
-	/** Check if Asmthry Enqueue class loaded properly */
-	if ( class_exists( 'Asmthry_Enqueue' ) ) {
-		if ( ! empty( $resources ) ) {
-			$enqueue = new Asmthry_Enqueue( $resources );
-			add_action( 'wp_enqueue_scripts', array( $enqueue, 'register_script' ) );
-			return $enqueue->get_ids();
-		}
-		return;
-	}
-}
-
-/** Enqueue Scripts
- *
- * @param array $ids - Give resources id as an array.
- */
-function asmthry_enqueue_script( array $ids = null ) {
-	Asmthry_Load_Resource::include_file( 'enqueue' );
-	/** Check if Asmthry Enqueue class loaded properly */
-	if ( class_exists( 'Asmthry_Enqueue' ) ) {
-		if ( ! empty( $ids ) ) {
-			$enqueue = new Asmthry_Enqueue( $ids );
-			add_action( 'wp_enqueue_scripts', array( $enqueue, 'enqueue_script' ) );
-		}
-	}
-}
-
 /** Enqueue Styles
  *
  * @param string $post_name - Give resources id as an array.
