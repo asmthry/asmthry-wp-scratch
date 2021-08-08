df2b5b02e1e2b5d07518da58b4ae847a6f21aebd
ASMTHRY <asmthry@gmail.com>
2021-08-08 14:24:49 +0530

    RESOURCES LOAD CLASS CHANGES 1) Pass class name to load resources.

--- global-functions.php
+++ global-functions.php
@@ -11,14 +11,11 @@
  * @param array $supports - Give support name as an array.
  */
 function asmthry_theme_support( array $supports = null ) {
-	Asmthry_Load_Resource::include_file( 'theme-support' );
-	/** Check if Asmthry Theme support class loaded properly */
-	if ( class_exists( 'Asmthry_Theme_Support' ) ) {
-		if ( ! empty( $supports ) ) {
-			new Asmthry_Theme_Support( $supports );
-		} else {
-			new Asmthry_Theme_Support();
-		}
+	Asmthry_Load_Resource::include_file( 'Asmthry_Theme_Support' );
+	if ( ! empty( $supports ) ) {
+		new Asmthry_Theme_Support( $supports );
+	} else {
+		new Asmthry_Theme_Support();
 	}
 }
 
